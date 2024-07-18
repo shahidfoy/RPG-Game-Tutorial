3706c589d7eb09947fa9580620b878e3335aeb96
Shahid Foy <shahidfoy@gmail.com>
2024-07-17 19:00:00 -0600

    first commit

---
+++ Assets/Scripts/Enemy/Skeleton/SkeletonBattleState.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkeletonBattleState : EnemyState
+{
+    private Transform player;
+    private Enemy_Skeleton enemy;
+    private int moveDirection;
+
+    public SkeletonBattleState(Enemy _enemyBase, EnemyStateMachine _stateMachine, string _animBoolName, Enemy_Skeleton _enemy) : base(_enemyBase, _stateMachine, _animBoolName)
+    {
+        this.enemy = _enemy;
+    }
+
+    public override void Enter()
+    {
+        base.Enter();
+        player = PlayerManager.instance.player.transform;
+    }
+
+    public override void Update()
+    {
+        base.Update();
+
+        if (enemy.IsPlayerDetected())
+        {
+            stateTimer = enemy.battleTime;
+            if (enemy.IsPlayerDetected().distance < enemy.attackDistance)
+            {
+                if (CanAttack())
+                    stateMachine.ChangeState(enemy.attackState);
+            }
+        }
+        else
+        {
+            if (stateTimer < 0 || Vector2.Distance(player.transform.position, enemy.transform.position) > 10)
+                stateMachine.ChangeState(enemy.idleState);
+        }
+
+
+        if (player.position.x > enemy.transform.position.x)
+            moveDirection = 1;
+        else if (player.position.x < enemy.transform.position.x)
+            moveDirection = -1;
+
+        enemy.SetVelocity(enemy.moveSpeed * moveDirection, rb.velocity.y);
+    }
+
+    public override void Exit()
+    {
+        base.Exit();
+    }
+
+    private bool CanAttack()
+    {
+        if (Time.time >= enemy.lastTimeAttacked + enemy.attackCoolDown)
+        {
+            enemy.lastTimeAttacked = Time.time;
+            return true;
+        }
+
+        return false;
+    }
+}
